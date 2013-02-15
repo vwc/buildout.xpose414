01c4e99e84d53efea14ff4af448b2f947b7ebd86
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-02-15 15:39:42 +0100

    Include newsletter subscribe form and add views

--- src/xpose.sitecontent/xpose/sitecontent/frontpage.py
+++ src/xpose.sitecontent/xpose/sitecontent/frontpage.py
@@ -1,9 +1,18 @@
 from five import grok
+from plone import api
 
 from plone.app.layout.navigation.interfaces import INavigationRoot
+
+from xpose.sitecontent.testimonial import ITestimonial
 
 
 class FrontPageView(grok.View):
     grok.context(INavigationRoot)
     grok.require('zope2.View')
     grok.name('frontpage-view')
+
+    def testimonials(self):
+        catalog = api.portal.get_tool(name="portal_catalog")
+        items = catalog(object_provides=ITestimonial.__identifier__,
+                        review_state="published")
+        return items
