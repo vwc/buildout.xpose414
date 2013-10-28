763a85129c7ff55ea653e4bad1ca52588f4f633b
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-10-28 18:06:09 +0100

    Refactor ga integration

--- src/xpose.workspaces/xpose/workspaces/workspace.py
+++ src/xpose.workspaces/xpose/workspaces/workspace.py
@@ -1,3 +1,4 @@
+import gdata
 from five import grok
 
 from z3c.form import group, field
@@ -42,3 +43,22 @@ class Analytics(grok.View):
     grok.context(IWorkspace)
     grok.require('zope2.View')
     grok.name('workspace-analytics')
+
+    def authorized(self):
+        """
+        Returns True if we have an auth token, or false otherwise.
+        """
+        if self.context.auth_token:
+            return True
+        return False
+
+    def auth_url(self):
+        """
+        Returns the URL used to retrieve a Google AuthSub token.
+        """
+        next = '%s/analytics-auth' % self.context.portal_url()
+        scope = 'https://www.google.com/analytics/feeds/'
+        return gdata.auth.GenerateAuthSubUrl(next,
+                                             scope,
+                                             secure=False,
+                                             session=True)
