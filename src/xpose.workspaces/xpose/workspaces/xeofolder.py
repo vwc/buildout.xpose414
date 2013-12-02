027ee1e232bf83328c4d6cef3eaa5cf20459fe47
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-12-03 00:52:04 +0100

    Integrate membrane with workspace content
    We use a dedicated membrane "content as user" approach - this allows
    for easy addition of new workspaces through staff members without
    mixing concerns

--- src/xpose.workspaces/xpose/workspaces/xeofolder.py
+++ src/xpose.workspaces/xpose/workspaces/xeofolder.py
@@ -1,22 +1,15 @@
+from Acquisition import aq_inner
+from AccessControl import Unauthorized
 from five import grok
 from plone import api
 
-from z3c.form import group, field
-from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from zope.component import getMultiAdapter
 
+from Products.CMFPlone.utils import safe_unicode
+from plone.keyring import django_random
+from plone.directives import form
 from plone.dexterity.content import Container
-from plone.directives import dexterity, form
-from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 from plone.namedfile.interfaces import IImageScaleTraversable
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
 
 from xpose.workspaces import MessageFactory as _
 
@@ -40,12 +33,20 @@ class View(grok.View):
     grok.name('view')
 
     def available_services(self):
-        keys = ('google', 'xovi', 'ac')
-        data = {}
-        for k in keys:
-            req_key = 'xeo.cxn.{0}_api_uri'.format(k)
+        services = {
+            u'google': _(u"Google Analytics"),
+            u'xovi': _(u"XOVI"),
+            u'ac': _(u"activeCollab"),
+        }
+        data = []
+        for s in services:
+            item = {}
+            req_key = 'xeo.cxn.{0}_api_uri'.format(s)
             api_uri = api.portal.get_registry_record(req_key)
-            data[k] = api_uri
+            item['name'] = services[s]
+            item['sid'] = s
+            item['uri'] = api_uri
+            data.append(item)
         return data
 
 
@@ -53,3 +54,56 @@ class CreateWorkspace(grok.View):
     grok.context(IXeoFolder)
     grok.require('cmf.ModifyPortalContent')
     grok.name('create-workspace')
+
+    def update(self):
+        context = aq_inner(self.context)
+        self.errors = {}
+        unwanted = ('_authenticator', 'form.button.Submit')
+        required = ('subject', 'email')
+        if 'form.button.Submit' in self.request:
+            authenticator = getMultiAdapter((context, self.request),
+                                            name=u"authenticator")
+            if not authenticator.verify():
+                raise Unauthorized
+            form = self.request.form
+            form_data = {}
+            form_errors = {}
+            errorIdx = 0
+            for value in form:
+                if value not in unwanted:
+                    form_data[value] = safe_unicode(form[value])
+                    if not form[value] and value in required:
+                        error = {}
+                        error['active'] = True
+                        error['msg'] = _(u"This field is required")
+                        form_errors[value] = error
+                        errorIdx += 1
+                    else:
+                        error = {}
+                        error['active'] = False
+                        error['msg'] = form[value]
+                        form_errors[value] = error
+            if errorIdx > 0:
+                self.errors = form_errors
+            else:
+                self._create_workspace(form)
+
+    def _create_workspace(self, data):
+        context = aq_inner(self.context)
+        new_title = data['title']
+        token = django_random.get_random_string(length=12)
+        item = api.content.create(
+            type='xpose.workspaces.workspace',
+            id=token,
+            title=new_title,
+            container=context,
+            safe_id=True
+        )
+        uuid = api.content.get_uuid(obj=item)
+        #item_id = item.getId()
+        #api.content.rename(obj=context[item_id],
+        #                   new_id=uuid)
+        url = context.absolute_url()
+        base_url = url + '/@@setup-workspace?uuid=' + uuid
+        next_url = base_url + '&token=' + token
+        return self.request.response.redirect(next_url)
