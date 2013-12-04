aebdd727472db9390acffb77a09ba5d74e840cac
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-12-04 16:04:52 +0100

    Prepare dashboard password provider

--- src/xpose.seodash/xpose/seodash/dashboard.py
+++ src/xpose.seodash/xpose/seodash/dashboard.py
@@ -1,3 +1,4 @@
+import re
 from five import grok
 
 from z3c.form import group, field
@@ -16,14 +17,77 @@ from plone.namedfile.interfaces import IImageScaleTraversable
 from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 
+from dexterity.membrane.membrane_helpers import validate_unique_email
 
 from xpose.seodash import MessageFactory as _
+
+
+def is_email(value):
+    """ Is this an email address? """
+    if not isinstance(value, str) or not '@' in value:
+        raise Invalid(_(u"Not an email address"))
+    return True
+
+
+def is_url(value):
+    """ Is this a URL? """
+    if isinstance(value, str):
+        pattern = re.compile(r"^https?://[^\s\r\n]+")
+        if pattern.search(value.strip()):
+            return True
+    raise Invalid(_(u"Not a valid link"))
 
 
 class IDashboard(form.Schema, IImageScaleTraversable):
     """
     A project dashboard
     """
+    email = schema.TextLine(
+        title=_(u"E-mail Address"),
+        required=True,
+        constraint=is_email,
+    )
+    first_name = schema.TextLine(
+        title=_(u"First Name"),
+        required=True,
+    )
+    last_name = schema.TextLine(
+        title=_(u"Last Name"),
+        required=True,
+    )
+    # Move unneeded parts to extra fieldset
+    form.fieldset(
+        'details',
+        label=_(u"Details"),
+        fields=['homepage', 'bio']
+    )
+    homepage = schema.TextLine(
+        # url format
+        title=_(u"External Homepage"),
+        required=False,
+        constraint=is_url,
+    )
+    form.widget(bio="plone.app.z3cform.wysiwyg.WysiwygFieldWidget")
+    bio = schema.Text(
+        title=_(u"Biography"),
+        required=False,
+    )
+
+    @invariant
+    def validateEmailUnique(data):
+        """The email must be unique, as it is the login name (user name).
+
+        The tricky thing is to make sure editing a user and keeping
+        his email the same actually works.
+        """
+        user = data.__context__
+        if user is not None:
+            if hasattr(user, 'email') and user.email == data.email:
+                # No change, fine.
+                return
+        error = validate_unique_email(data.email)
+        if error:
+            raise Invalid(error)
 
 
 class Dashboard(Container):
