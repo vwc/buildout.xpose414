5ad57f603627f6d4de24b2bdd707be39eb482578
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-12-04 15:33:37 +0100

    Refactor package layout

--- src/xpose.seodash/xpose/seodash/dashboard.py
+++ src/xpose.seodash/xpose/seodash/dashboard.py
@@ -20,48 +20,17 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from xpose.seodash import MessageFactory as _
 
 
-# Interface class; used to define content-type schema.
-
 class IDashboard(form.Schema, IImageScaleTraversable):
     """
     A project dashboard
     """
 
-    # If you want a schema-defined interface, delete the model.load
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/dashboard.xml to define the content type.
-
-    form.model("models/dashboard.xml")
-
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class Dashboard(Container):
     grok.implements(IDashboard)
 
-    # Add your class methods and properties here
 
-
-# View class
-# The view will automatically use a similarly named template in
-# dashboard_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
-class SampleView(grok.View):
-    """ sample view class """
-
+class View(grok.View):
     grok.context(IDashboard)
     grok.require('zope2.View')
-
-    # grok.name('view')
-
-    # Add view methods here
+    grok.name('view')
--- src/xpose.seodash/xpose/seodash/project.py
+++ src/xpose.seodash/xpose/seodash/project.py
@@ -27,41 +27,12 @@ class IProject(form.Schema, IImageScaleTraversable):
     A single project usually a single site
     """
 
-    # If you want a schema-defined interface, delete the model.load
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/project.xml to define the content type.
-
-    form.model("models/project.xml")
-
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class Project(Container):
     grok.implements(IProject)
 
-    # Add your class methods and properties here
-
-
-# View class
-# The view will automatically use a similarly named template in
-# project_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
 
 class SampleView(grok.View):
-    """ sample view class """
-
     grok.context(IProject)
     grok.require('zope2.View')
-
-    # grok.name('view')
-
-    # Add view methods here
+    grok.name('view')
