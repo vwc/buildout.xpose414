83c00e6e8b75ea6b36c964827620c8120e9d6d3a
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-01-15 12:28:50 +0100

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -15,9 +15,9 @@ env.port = '22222'
 env.user = 'root'
 env.hosts = ['6zu4']
 env.webserver = '/opt/webserver/buildout.webserver'
-env.code_root = '/opt/sites/xposeo/buildout.xposeo'
+env.code_root = '/opt/sites/xpose/buildout.xpose'
 env.local_root = '/Users/cb/dev/xpose/buildout.xpose'
-env.sitename = 'xposeo'
+env.sitename = 'xpose'
 env.code_user = 'root'
 env.prod_user = 'www'
 
